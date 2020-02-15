e02499f0913387f6527a46e3763663f43cdc2ef4
Bruno Mario Veronesi <endeios@gmail.com>
2020-02-15 23:02:07 +0100

    added cases till III

--- src/main/scala/romannumerals/RomanNumerals.scala
+++ src/main/scala/romannumerals/RomanNumerals.scala
@@ -1,5 +1,16 @@
 package romannumerals
 
 object RomanNumerals {
+  def convert(number: Int): String = {
+    val result = new StringBuilder("I")
+    if( number > 1){
+       result ++="I"
+    }
+    if (number > 2){
+      result ++= "I"
+    }
+
+    result.result()
+  }
 
 }
--- src/test/scala/romannumerals/RomanNumeralsTest.scala
+++ src/test/scala/romannumerals/RomanNumeralsTest.scala
@@ -1,11 +1,29 @@
 package romannumerals
 
 import org.scalatest._
+import org.scalatest.matchers.should.Matchers
 
-class RomanNumeralsTest extends FlatSpec{
+abstract class UnitTest extends FlatSpec with Matchers
+
+class RomanNumeralsTest extends UnitTest{
 
   "Roman numerals" should "exists" in {
     assert (RomanNumerals != null)
   }
+
+  case class Case(arabic: Int, roman: String)
+
+  val cases: List[Case] = List(
+    (1,"I"),
+    (2,"II"),
+    (3,"III"),
+  ).map(t=>Case(t._1,t._2))
+
+  cases.foreach(useCase=>{
+    it should s"transform ${useCase.arabic}  into ${useCase.roman}" in {
+      RomanNumerals.convert(useCase.arabic) shouldBe useCase.roman
+    }
+  })
+
 
 }
